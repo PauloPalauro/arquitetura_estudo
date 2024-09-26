cba082fa7f30cb8a35c8667af0ecb7962460cbb9
PauloEduardoPalauro <paulopalauro@gmail.com>
2024-09-26 18:39:01 -0300

    Microsserviço ate 4.3.52

--- apostila_microsserviços/lembretes/index.js
+++ apostila_microsserviços/lembretes/index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
 const bodyParser = require('body-parser');
+const axios = require("axios");
+
+
 app.use(bodyParser.json());
 
 
@@ -14,13 +17,28 @@ app.get ('/lembretes', (req, res) => {
     res.send(lembretes)
 });
 
-app.put ("/lembretes", (req, res) => {
+app.put ("/lembretes", async (req, res) => {
     contador++
     const { texto } = req.body; // Pega o corpo da requisição, pode ser usado sem as "{}"
     lembretes[contador] = {contador, texto}; //adicionando uma nova entrada no Objeto lembretes == lembretes[1], lembretes[2]
+
+    await axios.post("http://localhost:10000/eventos", {
+        tipo: "LembreteCriado",
+        dados: {
+            contador,
+            texto,
+        },
+    });
+
+    
     res.status(201).send(lembretes[contador]); // Envia a resposta ao cliente com o status HTTP 201 (Criado)
 
 });
+
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({mdsg: "ok"})
+})
 
 
 app.listen(4000, () => {
--- apostila_microsserviços/observacoes/index.js
+++ apostila_microsserviços/observacoes/index.js
@@ -1,11 +1,31 @@
 const express = require("express")
 const bodyParser = require('body-parser');
 const {v4: uuidv4} = require('uuid');
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 
 const observacoesPorLembreteId = {};
+
+const funcoes = {
+    ObservacaoClassificada: (observacao) => {
+        const observacoes = observacoesPorLembreteId[observacao.lembreteId];
+        const obsParaAtualizar = observacoes.find(o => o.id === observacao.id)
+        obsParaAtualizar.status = observacao.status;
+
+        axios.post("http://localhost:10000/eventos", {
+            tipo: "ObservacaoAtualizada",
+            dados: {
+                id: observacao.id,
+                texto: observacao.texto,
+                lembreteId: observacao.lembreteId,
+                status: observacao.status
+            }
+        })
+    }
+}
+
 
 /*
 :id é um placeholder
@@ -27,7 +47,7 @@ req.params.id -> pega o id que veio na URL (:id)
 "observacoesPorLembreteId[req.params.id] = observacoesDoLembrete;" 
     -> Após adicionar a nova observação, o array atualizado de observações é armazenado novamente no objeto observacoesPorLembreteId, associando-o ao id do lembrete.
 */
-app.put("/lembretes/:id/observacoes", (req, res) => {
+app.put("/lembretes/:id/observacoes", async (req, res) => {
 
     // Criando um uuid novo para a observacao
     const idObs = uuidv4();
@@ -35,12 +55,31 @@ app.put("/lembretes/:id/observacoes", (req, res) => {
 
     const observacoesDoLembrete = observacoesPorLembreteId[req.params.id] || [];
 
-    observacoesDoLembrete.push({id: idObs, texto}); 
+    observacoesDoLembrete.push({id: idObs, texto, status: "aguardando"}); 
 
     observacoesPorLembreteId[req.params.id] = observacoesDoLembrete;
 
+    await axios.post("http://localhost:10000/eventos", {
+        tipo : "ObservacaoCriada",
+        dados: {
+            id: idObs, 
+            texto, 
+            lembreteId: req.params.id,
+            status: "aguardando"
+        }
+    })
+
     res.status(201).send(observacoesDoLembrete);
 })
+
+app.post("/eventos", (req, res) => {
+    try{
+        funcoes[req.body.tipo](req.body.dados);
+    }
+    catch (err){}
+    res.status(200).send({mdsg: "ok"})
+})
+
 
 app.listen(5000, () => {
     console.log("Observações na Porta: 5000");
